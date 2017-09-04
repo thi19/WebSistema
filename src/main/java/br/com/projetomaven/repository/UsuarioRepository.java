ead8eb2dac303875e46c36e10c8bc9b86947a6ff
Thiago Souza <thiagotbs19@hotmail.com>
2017-09-04 19:37:15 -0300

    Update UsuarioRepository.java

--- src/main/java/br/com/projetomaven/repository/UsuarioRepository.java
+++ src/main/java/br/com/projetomaven/repository/UsuarioRepository.java
@@ -14,65 +14,26 @@ import br.com.projetomaven.model.UsuarioModel;
 @Repository
 public class UsuarioRepository {
  
-	/**
-	 * @PersistenceContext é o local onde ficam armazenados as entidades 
-	 * que estão sendo manipuladas pelo EntityManager
-	 * 
-	 * 
-	 *@PersistenceContext(type = PersistenceContextType.EXTENDED) assim o Wildfly vai 
-	 *gerenciar	para nos as entidades.
-	 * 
-	 **/
 	@PersistenceContext(type = PersistenceContextType.EXTENDED)
 	private EntityManager manager;
- 
- 
-	/**
-	 * 
-	 * @param usuarioModel
-	 * 
-	 * Salva um novo registro
-	 * 
-	 * O JPA exige um contexto de transação para realizar as alterações, por isso vamos
-	 * usar a anotação @javax.transaction.Transactional
-	 *  
-	 * */
+
 	@javax.transaction.Transactional
 	public void Salvar(UsuarioModel usuarioModel){
  
 		manager.persist(usuarioModel);		
 	}
  
-	/**
-	 * 
-	 * @param usuarioModel
-	 * 
-	 * Realiza a alteração de um registro
-	 */
 	@javax.transaction.Transactional
 	public void Alterar(UsuarioModel usuarioModel){
  
 		manager.merge(usuarioModel);		
 	}
  
-	/**
-	 * 
-	 * @param codigo
-	 * @return UsuarioModel
-	 * 
-	 * Consulta um usuário por código
-	 */	
 	public UsuarioModel ConsultarPorCodigo(int codigo){
  
 		return manager.find(UsuarioModel.class, codigo);		
 	} 
  
-	/**
-	 * 
-	 * @param codigo
-	 * 
-	 * Exclui um usuário pelo código
-	 */
 	@javax.transaction.Transactional
 	public void Excluir(int codigo){
  
@@ -81,13 +42,7 @@ public class UsuarioRepository {
 		manager.remove(usuarioModel);
  
 	}
- 
-	/**
-	 * 
-	 * @return List<UsuarioModel>
-	 * 
-	 * Consulta todos os usuários cadastrados no banco de dados
-	 */
+
 	public List<UsuarioModel> TodosUsuarios(){
  
 		return manager.createQuery("SELECT c FROM UsuarioModel c ORDER BY c.nome ", UsuarioModel.class).getResultList();	
